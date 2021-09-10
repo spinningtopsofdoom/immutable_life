4b79ec7fc5c7b905d4256dfbf575bd6d355ba27d
Peter Schuck <peter@bendyworks.com>
2021-09-10 11:44:27 -0500

    Add high level skeleton functions for life roadmap

--- src/life/multiverse/core.clj
+++ src/life/multiverse/core.clj
@@ -38,6 +38,74 @@
     :db/valueType   :db.type/long
     :db/cardinality :db.cardinality/one}])
 
+(defn starting-board
+  "Creates a new game populated game board"
+  []
+  #{})
+
+(defn modify-board
+  "creates a new game of life based off additions and removals of cells
+
+  Invariants
+  running modify board twice with the same add and remove gives the same board
+  the only cells in the modified board are in the original or add sets
+  Add overrides remove"
+  [board add remove]
+  board)
+
+(defn branch-board
+  "creates a new game of life based off of a current board at a specific time
+
+  Invariants
+  The branched board will be equal to the old board"
+  [board-time game-name new-game-name]
+  #{})
+
+(defn branch-modified-board
+  "creates a new game of life based off of a current board at a specific time
+
+  Invariants
+  The branched board will equal to the old board with modify-board ran on it"
+  [board-time game-name add remove new-game-name]
+  #{})
+
+(defn merge-game-boards
+  "Merges two game of life boards into a new board
+
+  Invariants
+  Cells in both boards will be in the merged board"
+  [board-a board-b]
+  board-a)
+
+(defn board-diff
+  "Gives the cells that were added and removed by two boards
+
+  Invariants
+  Diffing an empty board puts everything in added
+  Diffing the same board puts everything in removed"
+  [initial-board changed-board]
+  {:added #{} :removed #{}})
+
+(defn boards-equal
+  "checks if two boards are equal"
+  [board-a board-b]
+  false)
+
+(defn db->board
+  "Gets a game of life board given a game name"
+  [game-name db]
+  #{})
+
+(defn db-at-time->board
+  "Gets a game of life board given a game name at a given time"
+  [game-name game-time db]
+  #{})
+
+(defn game-history
+  "Gives the history of a particular game of life played out"
+  [game-name db]
+  [])
+
 (comment
   (def init-board #{})
 
